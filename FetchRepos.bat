62a5375149ff67a5a9240ac9796e84418a2e4060
Ruben Unteregger <ruben.unteregger@gmail.com>
2017-11-28 21:29:10 +0100

    Add AttackService to fetch script.

--- FetchRepos.bat
+++ FetchRepos.bat
@@ -4,6 +4,7 @@ SET MINARY_DIR=Minary
 SET PLUGINS_DIR=Plugins
 SET TOOLS_DIR=Tools
 SET LIBS_DIR=Libs
+SET ATTACKSERVICES_DIR=AttackServices
 
 :: Minary   -> Minary/
 IF NOT EXIST "%MINARY_DIR%" mkdir %MINARY_DIR% 2> nul
@@ -23,3 +24,9 @@ git clone git@github.com:Minary/Tools.git %TOOLS_DIR%
 :: Libs     -> Libs/
 IF NOT EXIST "%LIBS_DIR%" mkdir %LIBS_DIR% 2> nul
 git clone git@github.com:Minary/Libs.git %LIBS_DIR%
+
+
+:: AttackServices     -> AttackServices/
+IF NOT EXIST "%ATTACKSERVICES_DIR%" mkdir %ATTACKSERVICES_DIR% 2> nul
+git clone git@github.com:Minary/AttackServices.git %ATTACKSERVICES_DIR%
+
