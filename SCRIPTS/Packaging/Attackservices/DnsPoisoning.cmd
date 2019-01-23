43c37be7d0f3264dd5cc03bff87056f9d1984cca
Ruben Unteregger <ruben.unteregger@gmail.com>
2019-01-23 19:31:07 +0100

    Fix DnsPoisoning build scripts

--- SCRIPTS/Packaging/Attackservices/DnsPoisoning.cmd
+++ SCRIPTS/Packaging/Attackservices/DnsPoisoning.cmd
@@ -28,8 +28,8 @@ SET ATTACK_SERVICE_DIR=%3
 SET AS_DNSPOISONING_DIR=%3DnsPoisoning\
 SET OUTPUTFILE=outer.txt
 
-ECHO XCOPY /s /c /i /y AttackServices\ArpPoisoning\bin\%SOLUTIONCONFIG%\*.dll %AS_DNSPOISONING_DIR%
-XCOPY /s /c /i /y AttackServices\ArpPoisoning\bin\%SOLUTIONCONFIG%\*.dll %AS_DNSPOISONING_DIR% >>%OUTPUTFILE%
+ECHO XCOPY /s /c /i /y AttackServices\DnsPoisoning\bin\%SOLUTIONCONFIG%\*.dll %AS_DNSPOISONING_DIR%
+XCOPY /s /c /i /y AttackServices\DnsPoisoning\bin\%SOLUTIONCONFIG%\*.dll %AS_DNSPOISONING_DIR% >>%OUTPUTFILE%
 
 ECHO XCOPY /s /c /i /y Tools\DnsPoisoning\%SOLUTIONCONFIG%\bin\DnsPoisoning.exe %AS_APE_DIR%
 XCOPY /s /c /i /y Tools\DnsPoisoning\%SOLUTIONCONFIG%\bin\DnsPoisoning.exe %AS_APE_DIR% >>%OUTPUTFILE%
